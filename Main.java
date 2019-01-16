55cfa3bfc6e481ff1ffb5bb208084cf49b8a65b7
Emil <38663830+spirotris@users.noreply.github.com>
2019-01-16 18:36:50 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,9 @@
-import org.jibble.pircbot.*;
-import BotAnna.*;
-import java.util.*;
-import java.lang*;
-
-// Inklistrat från http://www.jibble.org/pircbot.php
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
         // Now start our bot up.
-        Bot bot = new Bot();
-
-        // Enable debugging output.
-        bot.setVerbose(true);
-
-        // Connect to the IRC server.
-        bot.connect("irc.freenode.net");
-
-        // Join the #pircbot channel.
-        bot.joinChannel("#pircbot");  // Ska vi skapa en egen channel eller hur fungerar IRC?
-
+        BotProject bot = new BotProject();
     }
 
 }
