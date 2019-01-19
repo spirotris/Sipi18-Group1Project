22cacd800978e5d760f3b1109d03f1e470592aac
spirotris <38663830+spirotris@users.noreply.github.com>
2019-01-19 12:16:54 +0100

    Deleted nbproject folder

---
+++ src/com/sipi/groupOne/Main.java
@@ -0,0 +1,11 @@
+import com.sipi.groupOne.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        // Now start our bot up.
+        BotProject bot = new BotProject();
+    }
+
+}
