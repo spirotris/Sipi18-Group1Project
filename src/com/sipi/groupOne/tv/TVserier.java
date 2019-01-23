315828326f73983cb6db280bf2fdca97ddd544a2
Marcizz <46536014+Marcizz@users.noreply.github.com>
2019-01-24 00:28:39 +0100

    Tagit bort onödig kod och sorterat mer. Gjort metoder mer modulära

--- src/com/sipi/groupOne/tv/TVserier.java
+++ src/com/sipi/groupOne/tv/TVserier.java
@@ -7,12 +7,13 @@
 package com.sipi.groupOne.tv;
 
 import java.net.*;
+import java.util.Iterator;
+
 import com.sipi.groupOne.connections.JSONCon;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-class TVserier {
-
+public class TVserier {
 
 	private static URL linkToAPI;
 	private static final String API = "API";
@@ -21,26 +22,44 @@ class TVserier {
 	private static final String SEARCH = "search/";
 	private static final String QUERY = "?q=";
 	private static final String SHOWS = "shows", PEOPLE = "people";
-	private static final String MANUAL = "Uses the API of " + NAME_OF_API_HOST_SITE + "to leverage results of querys, type API for more link. Example usage:\ntv show [name of tvshow]\ntv people [name of person]";
+	private static final String MANUAL = "Uses the API of " + NAME_OF_API_HOST_SITE
+			+ "to leverage results of querys, type API for more link. Example usage:\ntv show [name of tvshow]\ntv people [name of person]";
 	private static final String ERROR_NOT_ENOUGH_KEYWORDS = "Need more keywords to perform a search. Type ? or help for more information";
+	private static final String SHOW_MANUAL = "?", SHOW_MANUAL2 = "help";
+	private static final String EMPTY_RESULT = "Jag fick inga svar med den sökningen!";
+	private static final String TOO_MANY_PARAMETERS = "Har ej stöd för detta antal parametrar!";
 
+	private static final int FIRST_ARGUMENT = 1, SECOND_ARGUMENT = 2, THIRD_ARGUMENT = 3, FOURTH_ARGUMENT = 4,
+			MINIMUM_ARGUMENTS = 2, MIDDLE_VALUE_ARGUMENTS = 3, MAXIMUM_ARGUMENTS = 4;
 
 	private static String searchValue = "";
+	private static String result = "";
 	private static String show = "show";
+	
+	private static boolean isResultEmpty = true;
 
-	TVserier() {
+	public TVserier(String sender, String[] searchValue) {
 		try {
 			linkToAPI = new URL("https://api.tvmaze.com/");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
+		// Check and sort the parameters
+		sortSearch(searchValue);
+
+		String response = jsonResponse();
+		// TODO, när ska denna metod anropas?
+		
+		//Make sure the result is readable and looks good
+		formatResult(sender);
+
 	}
 
-	private static String jsonResponse() {
-		String json = linkToAPI + searchValue;
-		JSONCon tvmazeAPI = new JSONCon();
-		JSONArray response = tvmazeAPI.tryApi(json);
-		return String.format("Svaret är tomt, boten under arbete");
+	private boolean isResponseEmpty(JSONObject responseObject, JSONArray responseObjects) {
+		if (responseObject.get("Response").equals("False") || responseObjects.isEmpty())
+			return false;
+		else
+			return true;
 	}
 
 	/**
@@ -49,41 +68,80 @@ class TVserier {
 	 * @param keywords The specific keywords to use for the search
 	 * @return query through the API of @NAME_OF_HOST_SITE
 	 */
-	public static String search(String[] keywords) {
+	private void sortSearch(String[] keywords) {
 		// cast, episodes (nr of), characters, gallery (link to pictures), main link,
 		// get specific episode (by season nr and episode nr)
 		// show psych cast
 		/*
 		 * Switch on the following Show People (Person?) Seasons Episodes
 		 */
-		if (keywords.length <= 1) {
-			if (keywords[0] == "?" || keywords[0].toLowerCase().contains("help"))
-				return MANUAL;
-			else if (keywords[0].toUpperCase().contains(API))
-				return linkToAPI.toString();
-			return ERROR_NOT_ENOUGH_KEYWORDS;
-		}
-		else if (keywords.length == 2) {
-			if (keywords[0].toLowerCase().contains(show)) {
+		if (keywords.length <= MINIMUM_ARGUMENTS) {
+			if (keywords[FIRST_ARGUMENT] == SHOW_MANUAL
+					|| keywords[FIRST_ARGUMENT].toLowerCase().contains(SHOW_MANUAL2))
+				result = MANUAL;
+			else if (keywords[FIRST_ARGUMENT].toUpperCase().contains(API))
+				result = linkToAPI.toString();
+			result = ERROR_NOT_ENOUGH_KEYWORDS;
+		} else if (keywords.length == MIDDLE_VALUE_ARGUMENTS) {
+			if (keywords[FIRST_ARGUMENT].toLowerCase().contains(show)) {
 				// If entered string is something like [name of tv series] then return URL for
 				// that series
 				searchValue = SEARCH;
 				searchValue += QUERY;
 				searchValue += SHOWS;
-			} else if (keywords[0].toLowerCase().contains(PEOPLE)) {
+			} else if (keywords[FIRST_ARGUMENT].toLowerCase().contains(PEOPLE)) {
 				searchValue = SEARCH;
 				searchValue += QUERY;
 				searchValue += PEOPLE;
 			}
 
-		} else if (keywords.length == 3)
-			searchValue += keywords[0].toLowerCase() + EMBED + keywords[1];
+		} else if (keywords.length == MAXIMUM_ARGUMENTS)
+			searchValue += keywords[FIRST_ARGUMENT].toLowerCase() + EMBED + keywords[SECOND_ARGUMENT];
 		else
-			return "Unsupported number of keywords";
+			result = TOO_MANY_PARAMETERS;
 
-		String response = jsonResponse();
-
-		return response;
 	}
 
+	private String jsonResponse() {
+		String json = linkToAPI + searchValue;
+		JSONCon tvmazeAPI = new JSONCon();
+		JSONArray responseObjects = tvmazeAPI.tryApi(json);
+
+		// Picks the first row from the api-response
+		JSONObject responseObject = (JSONObject) responseObjects.get(0);
+		if (isResponseEmpty(responseObject, responseObjects))
+			return EMPTY_RESULT;
+		else {
+			isResultEmpty = false;
+			for (Object searchResults : responseObjects) {
+				JSONObject o = (JSONObject) searchResults;
+				System.out.println(o.toJSONString());
+				JSONArray resultsArr = (JSONArray) o.get("Search");
+				Iterator tvItr = resultsArr.iterator();
+				// Iterates through them to get the information i wan't
+				while (tvItr.hasNext()) {
+					Object slide = tvItr.next();
+					JSONObject tv = (JSONObject) slide;
+
+					// Detta behöver anpassas specifik för vad man har sökt på
+					// if (tv.get("Type").equals("movie"))
+					// jsonValue += tv.get("Title") + ", från " + movie.get("Year") + "; ";
+				}
+			}
+		}
+
+		return "";
+	}
+	
+	private void formatResult(String Sender)
+	{
+		if(isResultEmpty)
+			result = "Hej " + Sender + "!" + EMPTY_RESULT;
+		else
+			result = "Hej " + Sender + "! Jag hittade ";
+	}
+	
+	public String getAnswer() {		
+		return result;
+	}
 }
