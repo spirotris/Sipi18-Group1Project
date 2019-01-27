f954d1d2aaea3a0b8b58beac8963fb04e590da67
spirotris <38663830+spirotris@users.noreply.github.com>
2019-01-27 15:17:58 +0100

    Logfile test
    Simple way to pipe text into a file, requires all other functions to output error messages to System.err to avoid messing up the logfile.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import com.sipi.groupOne.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-
-        // Now start our bot up.
-        BotProject bot = new BotProject();
+    public static void main(String[] args) {
+        BotProject bot = new BotProject("port80a.se.quakenet.org", "Anna", "#group1-lernia", true);
     }
-
 }
--- src/com/sipi/groupOne/BotProject.java
+++ src/com/sipi/groupOne/BotProject.java
@@ -1,30 +1,46 @@
 package com.sipi.groupOne;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import org.jibble.pircbot.*;
 
 public class BotProject extends PircBot {
 
-    public BotProject() throws IOException {
-        this.setName("Anna");
+    private PrintStream ps;
+
+    public BotProject(String server, String nickname, String channel, boolean log) {
+        if (log) {
+            try {
+                System.setOut(new PrintStream(new File("logs/log.txt")));
+            } catch (FileNotFoundException e) {
+                System.err.println("Unable to create logfile");
+                System.err.println(e.getLocalizedMessage());
+            }
+        }
+
+        this.setName(nickname);
         this.setVerbose(true);
         try {
-            this.connect("port80a.se.quakenet.org");
-        } catch (IrcException ex) {
-            System.out.println(ex.getLocalizedMessage());
+            this.connect(server);
+        } catch (IrcException e) {
+            System.err.println("IRC Exception: \n" + e.getLocalizedMessage());
+        } catch (IOException e) {
+            System.err.println("IO Exception: \n" + e.getLocalizedMessage());
         }
-        this.joinChannel("#group1-lernia");
+        this.joinChannel(channel);
     }
 
     // Scanning the chat in the channel for commands
     @Override
     public void onMessage(String channel, String sender,
-                          String login, String hostname, String message) {
+            String login, String hostname, String message) {
         // Getting the result
-        String searchResult = ChannelScreener.message(sender,message);
+        String searchResult = ChannelScreener.message(sender, message);
 
         // Checking that the answer is null, if it is it's not a command for Anna and she shouldn't do a thing
-        if(searchResult != null) {
+        if (searchResult != null) {
             sendMessage(channel, searchResult);
         }
     }
