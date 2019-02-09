4266031d77bcea191bf3c0c407cfd7b70d817aba
spirotris <38663830+spirotris@users.noreply.github.com>
2019-02-09 17:23:54 +0100

    Update Main.java
    Another argument added to the bot, boolean for saving logfile or not.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import com.sipi.groupOne.ui.MainFrame;
 public class Main {
 
     public static void main(String[] args) {
+        BotProject bot;
+        Thread t = new Thread(bot = new BotProject("port80a.se.quakenet.org", "[GUI]Anna", "#group1-lernia", true));
         // String[] args for cmdline or GUI?
-        MainFrame ui = new MainFrame();
-        ui.run();
+        MainFrame ui = new MainFrame(bot);
+        bot.setUi(ui);
+        t.start();
     }
 }
