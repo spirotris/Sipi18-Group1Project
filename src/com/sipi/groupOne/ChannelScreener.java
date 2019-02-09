7eeb760a3546552cd28f759e1a6e04e6abb24a49
spirotris <38663830+spirotris@users.noreply.github.com>
2019-02-09 20:34:23 +0100

    Update ChannelScreener.java
    General cleanup and improved readability of code. Added hostname as input to GPS-function.

--- src/com/sipi/groupOne/ChannelScreener.java
+++ src/com/sipi/groupOne/ChannelScreener.java
@@ -2,29 +2,18 @@ package com.sipi.groupOne;
 
 import com.sipi.groupOne.movie.FetchMovieInfo;
 import com.sipi.groupOne.gps.GetGPS;
-import com.sipi.groupOne.test.FetchTV;
-import com.sipi.groupOne.tv.TVserier;
-
-
 
 public class ChannelScreener {
 
-    public static String message(String sender, String msg) {
+    public static String message(String sender, String hostname, String msg) {
         // Cleaning up the chat.message to find a command to process
         // Split the message to an array of the words to extract the command
         String[] msgArray = msg.split(" ");
         switch (msgArray[0].toLowerCase()) {
-            case "time":
-                String time = new java.util.Date().toString();
-                return sender + ": The time is now " + time;
             case "movie":
-                // To call the omdb api and get some movie-info
-                FetchMovieInfo movie = new FetchMovieInfo(sender, searchString(msgArray));
-                return movie.getAnswer();
+                return new FetchMovieInfo(sender, searchString(msgArray)).getAnswer();
             case "gps":
-                GetGPS gps = new GetGPS(sender,searchString(msgArray));
-                return gps.getInfo();
-           
+                return new GetGPS(sender, hostname).getInfo();
         }
 
         return null;
@@ -35,10 +24,11 @@ public class ChannelScreener {
     private static String searchString(String[] searchArr) {
         StringBuilder str = new StringBuilder();
         for (int i = 1; i < searchArr.length; i++) {
-            if((searchArr.length -1) != i)
-                str.append(searchArr[i] + " ");
-            else
+            if ((searchArr.length - 1) != i) {
+                str.append(searchArr[i]).append(" ");
+            } else {
                 str.append(searchArr[i]);
+            }
         }
         return str.toString();
     }
