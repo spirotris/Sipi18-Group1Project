79fe01f2d2e360ef62a65430e81a2608b013793b
spirotris <38663830+spirotris@users.noreply.github.com>
2019-02-12 19:52:22 +0100

    Demo of remote control.

---
+++ src/com/sipi/groupOne/remotecontrol/BotClient.java
@@ -0,0 +1,26 @@
+package com.sipi.groupone.remotecontrol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class BotClient {
+
+    public void start(int port) throws IOException, InterruptedException {
+        Socket clientSocket = new Socket("127.0.0.1", port);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine = "";
+        while (true) {
+            inputLine = in.readLine();
+            if(!inputLine.equals(in.readLine())) {
+             System.out.println(inputLine);   
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new BotClient().start(6668);
+    }
+}
