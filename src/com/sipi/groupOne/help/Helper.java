685a14fc7fb0578c61cf460cf5e900be15fc811f
Marcizz <46536014+Marcizz@users.noreply.github.com>
2019-02-13 19:08:01 +0100

    Update Helper.java
    Fixat msgArray typen till Array.

--- src/com/sipi/groupOne/help/Helper.java
+++ src/com/sipi/groupOne/help/Helper.java
@@ -12,13 +12,14 @@ public class Helper {
     private final String HOWTO = " en sökning är uppbyggd på följande sätt: <Vad du söker> <Sök-kommando>. " + time + ", " + omdbApi + ", " + tvMaze + ", " + gpsApi;
     private static final String HELLO = "Hej ";    
     
-    String sender, msgArray;
+    String sender;
+    String[] msgArray;
     
-    public Helper(String sender, String msgArray) {
+    public Helper(String sender, String[] msgArray) {
     	this.sender = sender;
     	
     	//Vet inte vad denna ska användas till än, men kan vara bra att ha
-    	this.msgArray = msgArray; 
+    	this.msgArray = msgArray;
     }
 
     public String getAnswer() {
