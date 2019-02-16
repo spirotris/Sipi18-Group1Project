e4927e13d51118a52ea5a3e81c9f356aa65108fb
bob <mahboobabutt@gmail.com>
2019-02-16 14:43:02 +0100

    Update GetGPS.java

--- src/com/sipi/groupOne/gps/GetGPS.java
+++ src/com/sipi/groupOne/gps/GetGPS.java
@@ -9,33 +9,25 @@ package com.sipi.groupOne.gps;
  *
  * @author mbutt
  */
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.nio.charset.Charset;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.json.simple.JSONObject;
-//import org.json.simple.JSONArray;
 
 public class GetGPS {
-    
-  OkHttpClient client = new OkHttpClient();
-  
-  String run(String url) throws IOException {
-    Request request = new Request.Builder()
-        .url(url)
-        .build();
-
-    try (Response response = client.newCall(request).execute()) {
-      return response.body().string();
-    }
-  }
-  
-  private String jsonvalue;
+  private JSONObject json;
+  private final String URL_IN = "https://freegeoip.app/json/";
+  private StringBuilder jsonvalue;
   private String SEARCHVALUE;
   private String SENDER;
-  private boolean run = false;
+  private boolean run = false;  
+  
   
   // Contructor
     public GetGPS(){
@@ -45,38 +37,51 @@ public class GetGPS {
     public GetGPS(String sender, String searchValue) {
         SEARCHVALUE = searchValue;
         SENDER = sender;
-        
         if(!run == true){
             run = true;
             GPSinfo();
         }
     }
+ 
   
+  private String readAll (Reader read) throws IOException{
+      int count;
+      StringBuilder value = new StringBuilder();
+      while ((count = read.read()) != -1){
+          value.append((char)count);
+      }
+      return value.toString();
+  } 
+  
+  private JSONObject readJsonFromUrl(String url){
     
-    // public String
+      JSONParser parser = new JSONParser();
+    
+      
+      try (InputStream inputstrm = new URL(url).openStream()){
+      BufferedReader buffread = new BufferedReader(new InputStreamReader(inputstrm, Charset.forName("UTF-8")));    
+      String jsontext = readAll(buffread);
+    
+     json = (JSONObject)parser.parse(jsontext);     
+      }catch(ParseException | IOException e){
+          System.out.println(e.getMessage());
+      }
+      return json;     
+  }  
+    
   private void GPSinfo(){
-   //GetGPS example = new GetGPS(SENDER,SEARCHVALUE);
-    GetGPS example = new GetGPS();
-    JSONParser parser = new JSONParser();
-    try{
-
-    //Assigns the json out in a string variabel "response"    
-    String response = example.run("https://freegeoip.app/json/");
-    
-    //Pasring the JSON string into a JSON Object 
-    JSONObject jsobjt = (JSONObject)parser.parse(response);
- 
-    jsonvalue = ("Hej "+SENDER+ ", Du har IP :"+jsobjt.get("ip")+", bor i landet "+jsobjt.get("country_name")
-                +" i regionen "+jsobjt.get("region_name")+" i staden "+jsobjt.get("city")
-                +" med postnummer "+jsobjt.get("zip_code"));
- 
-    }catch(ParseException | IOException e){
-        System.out.println(e.getMessage());  
+      json = readJsonFromUrl(URL_IN);
+      String jsonvalue_tmp = ("Hej "+SENDER+ ", Du har IP :"+json.get("ip")+", bor i landet "+json.get("country_name")
+              +" i regionen "+json.get("region_name")+" i staden "+json.get("city")
+              +" med postnummer "+json.get("zip_code"));
+      jsonvalue = new StringBuilder(jsonvalue_tmp);
+  } 
+  
+  public String getInfo() {
+        
+        return jsonvalue.toString();
     }
+
   }
     
-    public String getInfo() {
-        
-        return jsonvalue;
-    }
-}
+
